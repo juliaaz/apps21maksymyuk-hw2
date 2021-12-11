8cfb33151f0dc07c2da99bd8278318dba01c8ab7
Julia Maksymiuk <Julia@MacBook-Pro-Julia-2.local>
2021-12-11 02:55:46 +0200

    uploaded modified tests

--- src/test/java/ua/edu/ucu/collections/StackTest.java
+++ src/test/java/ua/edu/ucu/collections/StackTest.java
@@ -2,31 +2,62 @@ package ua.edu.ucu.collections;
 
 import org.junit.Before;
 import org.junit.Test;
+import ua.edu.ucu.collections.immutable.ImmutableLinkedList;
 
 import static org.junit.Assert.*;
 
 public class StackTest {
     private Stack stack;
+    private Stack emptyStack;
 
     @Before
     public void setUp() {
-        stack = new Stack();
-        for (int i : new int[]{1, 2, 3, 4, 5}) {
-            stack.push(i);
-        }
+        Object[] array = {1, 3, 5, 7};
+        ImmutableLinkedList linkedList = new ImmutableLinkedList(array);
+        this.stack = new Stack(linkedList);
+        this.emptyStack = new Stack(new ImmutableLinkedList());
     }
 
     @Test
-    public void pop() {
-        assertEquals(5, stack.pop());
-        assertEquals(4, stack.pop());
-        assertEquals(3, stack.pop());
-        assertEquals(2, stack.pop());
-        assertEquals(1, stack.pop());
+    public void testPeek() {
+        Object expResult = 1;
+        Object actualResult = stack.peek();
+
+        assertEquals(expResult, actualResult);
     }
 
     @Test
-    public void peek() {
-        assertEquals(5, stack.peek());
+    public void testPeekEmpty() {
+        assertNull(emptyStack.peek());
     }
+
+    @Test
+    public void testPop() {
+        Object expResult = 1;
+        Object actualResult = stack.pop();
+
+        assertEquals(expResult, actualResult);
+    }
+
+    @Test(expected = ArrayIndexOutOfBoundsException.class)
+    public void testPopEmpty() {
+        assertNull(emptyStack.pop());
+    }
+
+    @Test
+    public void testPush() {
+        Object[] expResult = {9, 1, 3, 5, 7};
+        stack.push(9);
+
+        assertArrayEquals(expResult, stack.getStack().toArray());
+    }
+
+    @Test
+    public void testEnqueueEmpty() {
+        Object[] expResult = {9};
+        emptyStack.push(9);
+
+        assertArrayEquals(expResult, emptyStack.getStack().toArray());
+    }
+
 }
