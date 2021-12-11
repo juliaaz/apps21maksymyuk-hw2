ea4e1d0caa0e9606cbea48f63755e2314bc5cc6c
Julia Maksymiuk <Julia@MacBook-Pro-Julia-2.local>
2021-12-11 02:53:44 +0200

    uploaded modified code

--- src/main/java/ua/edu/ucu/collections/Stack.java
+++ src/main/java/ua/edu/ucu/collections/Stack.java
@@ -1,14 +1,29 @@
 package ua.edu.ucu.collections;
 
+import lombok.Getter;
+import ua.edu.ucu.collections.immutable.ImmutableLinkedList;
+@Getter
 public class Stack {
-    public void push(Object e) {
-    }
+    private ImmutableLinkedList stack;
 
-    public Object pop() {
-        return null;
+
+    public Stack(ImmutableLinkedList newStack) {
+        this.stack = newStack;
     }
 
     public Object peek() {
-        return null;
+        return stack.getFirst();
     }
+
+    public Object pop() {
+        Object last;
+        last = stack.getFirst();
+        stack = stack.removeFirst();
+        return last;
+    }
+
+    void push(Object e) {
+        stack = stack.addFirst(e);
+    }
+
 }
